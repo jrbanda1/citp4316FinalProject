c5e0cb4e5948cd85e542b6aea1aaff5f6aa87842
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-11-04 21:12:18 -0500

    fixing 404 error not working properly

--- src/models/companies.js
+++ src/models/companies.js
@@ -1,5 +1,13 @@
-import { db } from '../utils/db'
+import db from '../utils/db'
 
 export const getCompanies = async () => db.company.findMany()
 
-export const getCompany = async (id) => db.company.findUnique({where:{id}})
+export const getCompany = async (id) =>
+  db.company.findUnique({
+    where: { companyId: id },
+    include: {
+      departments: true,
+    },
+  })
+
+export const addCompany = async (name) => db.company.create({ data: { name } })
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,18 +1,9 @@
 import { Router } from 'express'
-import basicAuth from 'express-basic-auth'
 
 import persons from './persons'
 import companies from './companies'
-import departments from './departments'
-import employees from './employees'
 
 const router = Router()
-
-router.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
-  }),
-)
 
 router.get('/', (req, res) => {
   res.send({ msg: 'Inside API Endpoints' })
@@ -20,7 +11,5 @@ router.get('/', (req, res) => {
 
 router.use('/persons', persons)
 router.use('/companies', companies)
-router.use('/departments', departments)
-router.use('/employees', employees)
 
 export default router
