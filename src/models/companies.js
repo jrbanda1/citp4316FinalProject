2cf70a3bf1a994bc66738846daf1f114fa0bc8ba
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-11-04 21:15:51 -0500

    fixed 404 error not working

--- src/models/companies.js
+++ src/models/companies.js
@@ -3,11 +3,6 @@ import db from '../utils/db'
 export const getCompanies = async () => db.company.findMany()
 
 export const getCompany = async (id) =>
-  db.company.findUnique({
-    where: { companyId: id },
-    include: {
-      departments: true,
-    },
-  })
+  db.company.findUnique({ where: { companyId: id } })
 
 export const addCompany = async (name) => db.company.create({ data: { name } })
