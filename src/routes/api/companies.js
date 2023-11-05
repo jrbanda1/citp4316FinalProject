421791f54bccda59151ad95a46adc180d5a5cdd5
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-11-04 21:22:35 -0500

    fixed 404 44:34

--- src/routes/api/companies.js
+++ src/routes/api/companies.js
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 
 import { getCompanies, getCompany, addCompany } from '../../models/companies'
-import { addDepartment } from '../../models/departments'
 
 const router = Router()
 
@@ -30,18 +29,12 @@ router.post('/', async (req, res) => {
 })
 
 router.post('/:id', async (req, res) => {
-  const companyId = req.params.id
-  const company = await getCompany(companyId)
+  const companyId = req.body.name
   if (company) {
-    const departmentName = req.body.name
-    if (departmentName) {
-      const department = await addDepartment(departmentName, companyId)
-      res.send(department)
-    } else {
-      res.status(400).send({ msg: 'Department name is required' })
-    }
-  } else {
-    res.status(400).send({ msg: 'Company does not exist required' })
+    const company = await addCompany(companyName)
+    res.send(company)
+  }else {
+    res.status(400).send({msg: 'Compnay name is Required'})
   }
 })
 
