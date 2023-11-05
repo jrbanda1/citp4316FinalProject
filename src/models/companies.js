f8487239221dcd454368c6ea381628d574bd6572
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-11-04 19:47:32 -0500

    companies.js as in 31:29

--- src/models/companies.js
+++ src/models/companies.js
@@ -1,13 +1,7 @@
-import db from '../utils/db'
+import { db } from '../utils/db'
 
-export const getCompanies = async () => db.company.findMany()
+const getCompanies = async () => db.company.findMany()
 
-export const getCompany = async (id) =>
-  db.company.findUnique({
-    where: { companyId: id },
-    include: {
-      departments: true,
-    },
-  })
-
-export const addCompany = async (name) => db.company.create({ data: { name } })
+export default{
+  getCompanies,
+}
