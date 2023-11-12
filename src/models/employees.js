6ae90ae5b74a698527d79ce23c4233575eb0ba01
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-11-12 09:21:14 -0600

    video part 3 5:14 employee.js file

--- src/models/employees.js
+++ src/models/employees.js
@@ -1,30 +1,18 @@
 import db from '../utils/db'
 
-export const getEmployees = async (skip, take) => {
-  const count = await db.employee.count()
-  const employees = await db.employee.findMany({
-    skip,
-    take,
-  })
-  return { count, employees }
-}
+export const getEmployees - async () => db.employee.findMany()
 
 export const getEmployee = async (id) =>
   db.employee.findUnique({ where: { employeeId: id } })
 
-export const addEmployee = async (employeeData) =>
-  db.employee.create({ data: { ...employeeData } })
+export const addEmployee = async (name, departmentId) =>
+  db.employee.create({ data: { name, departmentId } })
 
-export const updateEmployee = async (id, employeeData) => {
-  const employee = await getEmployee(id)
-  if (employee) {
-    return db.employee.update({
-      where: { employeeId: id },
-      data: { ...employee, ...employeeData, updatedAt: new Date() },
-    })
-  }
-  return null
-}
+export const updateEmployee = async (id, name, departmentId ) => {
+ db.employee.update({
+  where:{ employeeId: id},
+  data: {name, departmentId},
+ })
 
 export const deleteEmployee = async (id) =>
   db.employee.delete({ where: { employeeId: id } })
