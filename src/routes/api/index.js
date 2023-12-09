5d82a5427cbfa646d0ae761ece0056d098a83f8c
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-12-08 20:29:33 -0600

    corrected undefined variable countries/cities

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,8 @@
 import { Router } from 'express'
 import basicAuth from 'express-basic-auth'
 
-import persons from './persons'
-import companies from './companies'
-import departments from './departments'
-import employees from './employees'
+import countries from './countries'
+import cities from './cities'
 
 const router = Router()
 
@@ -18,9 +16,7 @@ router.get('/', (req, res) => {
   res.send({ msg: 'Inside API Endpoints' })
 })
 
-router.use('/persons', persons)
-router.use('/companies', companies)
-router.use('/departments', departments)
-router.use('/employees', employees)
+router.use('/countries', countries)
+router.use('/cities', cities)
 
 export default router
